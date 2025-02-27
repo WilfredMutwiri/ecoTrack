b09bdf78d82aac744c9d8457de8a7a17a2b205cb
Wilfred Mutwiri <wilfredmutwiri20@gmail.com>
2025-02-27 11:48:00 +0300

    added about us page

--- ecotrack/src/pages/About.jsx
+++ ecotrack/src/pages/About.jsx
@@ -5,6 +5,7 @@ import caleb from '../assets/caleb.jpeg';
 import wilfred from '../assets/wilfred.jpeg';
 import gillo from '../assets/gillo.jpg'
 import brandon from '../assets/brandon.jpg'
+import banner2 from '../assets/banner2.jpg';
 
 export default function About() {
     const teamData=[
@@ -39,48 +40,49 @@ export default function About() {
     ]
    return (
         <div className="w-full bg-gray-50 ">
-            <div className="block md:flex gap-5 w-11/12 md:w-10/12 pt-16 mx-auto">
-            {/* <div className='flex-1'>
-            </div> */}
-            <div className='flex-1 leading-relaxed'>
-                <h2 className='font-bold text-2xl text-green-600'>Welcome to EcoTrack Africa.
-                </h2>
+            <div className="block gap-5 w-11/12 md:w-10/12 mx-auto">
+            <div className='flex-1'>
+                <img src={banner2} className='w-[100%] h-96 object-cover'/>
+            </div>
+            <div className='flex-1 leading-relaxed pt-10'>
+                <h2 className='font-bold text-3xl text-green-600 text-center'>About <span className="text-black">LISHE</span>Track</h2>
                 <div className='flex flex-col gap-2'>
-                    <p>
-                    At EcoTrack Africa,we are dedicated to provide a platform for environmental monitoring and waste management across the continent. Our advanced AI technologies enable real-time analytics, waste hotspot tracking, and air pollution monitoring, empowering communities to take actionable steps towards sustainability. Join us in our mission to create a cleaner, greener Africa.
+                    <p className='text-center leading-relaxed'>
+                    At LISHETrack, we are committed to revolutionizing the biofortified crop supply chain through technology, transparency, and trust. Our platform provides real-time tracking, AI-powered quality verification, and smart market linkages, ensuring farmers, buyers, and regulators can seamlessly verify, trade, and manage biofortified crops with confidence.
                     </p>
-                    <h2 className='font-bold text-2xl text-green-600'>Our Mission:</h2>
-                    <p>
-                    EcoTrack Africa is a smart waste management platform that uses AI, satellite technology, and real-time data to help African cities optimize waste collection, reduce emissions, and promote sustainability—accessible via webapp and mobile applications for city planners, urban developers, and traffic engineers. .
-                    </p>
-                    <h2 className='font-bold text-2xl text-green-600'>Our Vission:</h2>
-                    <p>
-                    To be the leading platform in Africa for environmental monitoring, providing tools and resources that empower individuals and organizations to make a positive impact on their surroundings.
-                    </p>
-                    <h2 className='font-bold text-2xl text-green-600'>Our Values:</h2>
-                    <ul>
-                        <li>Sustainability: Committed to promoting eco-friendly practices.</li>
-                        <li>Innovation: Leveraging technology to solve environmental challenges.</li>
-                        <li>Community: Engaging and supporting local communities in their sustainability efforts.</li>
-                    </ul>
+
+                    <div className='flex flex-row gap-4 pt-10'>
                     <div>
-                        <h2 className='text-center text-green-600 font-semibold text-2xl pt-3 pb-3'>Meet Our Team</h2>
-                        <div className='flex flex-row gap-7 p-3 pt-4 pb-10'>
-                            {
-                                teamData.map((member,index)=>(
-                                    <div key={index} className=' w-[240px]'>
-                                        <img src={member.image} className='h-[250px] w-[400px] object-contain'/>
-                                        <div className='mx-auto p-2 bg-gray-100'>
-                                        <p className='font-semibold text-green-700'>{member.name}</p>
-                                        <p className='text-sm text-cyan-700'>{member.role}</p>
-                                        <p>Tel: {member.phone}</p>
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
+                        <h2 className='font-bold text-2xl text-green-600'>Our Mission:</h2>
+                        <p>
+                        To create a transparent, efficient, and technology-driven agricultural ecosystem that ensures biofortified crops are properly identified, segregated, and distributed to improve nutrition, market accessibility, and food security.
+                        </p>
                     </div>
 
+                    <div>
+                    <h2 className='font-bold text-2xl text-green-600'>Our Vission:</h2>
+                    <p>
+                    To be Africa’s leading digital platform for traceability and market aggregation, making nutrient-rich, biofortified crops more accessible and valuable for both farmers and consumers.
+                    </p>
+                    </div>
+
+                    </div>
+                    <h2 className='font-bold text-2xl text-green-600 pt-10'>Why LISHETrack?</h2>
+                    <ul>
+                        <li>Enhancing Trust & Transparency – Eliminates fraud in biofortified crop trading.</li>
+                        <li>Empowering Farmers – Provides digital tools for better market access.</li>
+                        <li>Promoting Health & Nutrition – Encourages large-scale adoption of nutrient-rich crops.</li>
+                        <li>Driving Innovation – Uses AI, blockchain, and geo-mapping for improved supply chain efficiency.</li>
+                    </ul>
+                    <div>
+                        <h2 className='font-bold text-2xl text-green-600 pt-10'>Join Us in Transforming Agriculture!</h2>
+                        <p>
+                        LISHETrack is more than just a platform—it’s a movement towards smarter, healthier, and more profitable farming. Whether you are a farmer, buyer, or stakeholder, LISHETrack is your trusted partner for sustainable agricultural growth
+                        </p>
+                        <Link to="/dashboard">
+                            <Button className="w-11/12 md:w-56 mt-14 mx-auto" gradientDuoTone="greenToBlue">Get Started</Button>
+                        </Link>
+                    </div>
                 </div>
             </div>
             </div>
